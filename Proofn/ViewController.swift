fac2469b144695cad8a9db0ebf155a44908fbafb
Wildan Angga Rahman <wildan.aga@gmail.com>
2019-05-04 12:22:01 +0700

    Initial Commit

---
+++ Proofn/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Proofn
+//
+//  Created by Wildan Angga Rahman on 5/4/19.
+//  Copyright © 2019 Wildan Angga Rahman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ProofnTests/ProofnTests.swift
@@ -0,0 +1,34 @@
+//
+//  ProofnTests.swift
+//  ProofnTests
+//
+//  Created by Wildan Angga Rahman on 5/4/19.
+//  Copyright © 2019 Wildan Angga Rahman. All rights reserved.
+//
+
+import XCTest
+@testable import Proofn
+
+class ProofnTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
